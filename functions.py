f9cf4a4bd73a06e4a1e1d0012b1905b0bb9966db
alu0101028163 <alu0101028163@ull.edu.es>
2019-04-10 19:57:24 +0100

    Added recalculate_centroid function, and tests passed. 1 decimal by defect.

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
-import math     # For using sqrt
-
+import math             # For using sqrt
+import numpy as np      # Arrays for high performance operations
 
 def euclid_distance(point_a, point_b):
     if(len(point_a) != len(point_b)):
@@ -8,3 +8,9 @@ def euclid_distance(point_a, point_b):
     for coordinate_a, coordinate_b in zip(point_a, point_b):
         summation += pow(coordinate_a - coordinate_b , 2)
     return math.sqrt(summation)
+
+def recalculate_centroid(points):
+    summation = np.zeros(len(points[0]))
+    for point in points:
+        summation = np.add(summation, point)
+    return np.round((summation / len(points)), 1)
--- test_functions.py
+++ test_functions.py
@@ -1,5 +1,7 @@
 import pytest
 import functions
+import numpy as np      # Arrays for high performance operations
+
 
 
 def test_euclid_distance():
@@ -7,3 +9,11 @@ def test_euclid_distance():
     assert round(functions.euclid_distance([1,2],[3,4]),3) == 2.828
     assert round(functions.euclid_distance([1.5, 2.3],[1.5, 2.3]), 3) == 0.000
     assert round(functions.euclid_distance([1.5, 2.3],[2.3, 1.5]), 3) == 1.131
+
+def test_recalculate_centroid():
+    cluster_a = [[1,1],[2,4]]
+    cluster_b = [[1,2,3],[4,5,6],[8,9,10]]
+    cluster_c = [[-2,4,5,-8],[1,-2,-2,4]]
+    assert(np.array_equal(functions.recalculate_centroid(cluster_a), [1.5, 2.5]))
+    assert(np.array_equal(functions.recalculate_centroid(cluster_b), [4.3,5.3,6.3]))
+    assert(np.array_equal(functions.recalculate_centroid(cluster_c), [-0.5,1,1.5,-2]))
